f98d45b1677daaf75caa951f4d635b5de6450fe0
magnetenstad <tenstad.magne@gmail.com>
2021-06-16 22:49:10 +0200

    Add static adapter and fix deploy folder name

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,26 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
+	
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		// Comment the paths if wants to run in dev mode.
+		paths: {
+			base: '/drumhero',
+			assets: '/drumhero'
+		}
 	}
 };
 
