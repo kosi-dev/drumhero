a49dc482fcc6a9a82af7eed932618216674eb751
magnetenstad <tenstad.magne@gmail.com>
2021-06-17 02:34:40 +0200

    Add sfx

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,14 +2,22 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 
-	let tracks: string[] = [""];
+	let tracks: string[] = [
+		"1010101010001010",
+		"0000000010000000",
+		"0010010000010101",
+		"10001",
+		"00001",
+	];
 	let bpm: number = 60;
-	let bpb: number = 1;
+	let bpb: number = 4;
 	let interval: NodeJS.Timeout;
-	let timer: number = 0
+	let timer: number = 0;
+	let audios: string[] = ["hit", "coin", "blop", "explode",  "powerup"];
+	let playing: boolean = false;
 
 	function addTrack() {
-		tracks = [...tracks, ""]
+		tracks = [...tracks, ""];
 	}
 
 	function removeTrack(i: number) {
@@ -17,33 +25,44 @@
 		tracks = tracks;
 	}
 
-	function resetTimer() {
+	function togglePlaying() {
+		playing = !playing;
 		timer = 0;
 	}
 
 	function appendTimer() {
-		var max = tracks.flatMap((track) => track.length).sort()[tracks.length-1]
-		if (max == 0) {
-			return
+		if (!playing) {
+			return;
 		}
+
+		var max = tracks.flatMap((track) => track.length).sort((a, b) => a - b)[tracks.length-1];
+		if (max == 0) {
+			return;
+		}
+
 		timer = (timer + 1) % max
-		var audio = new Audio('click.wav');
-		tracks.forEach((track) => {
-			if (track.charAt(timer) == '1') {
-				audio.play()
+		
+		for (let i = 0; i < Math.min(tracks.length, audios.length); i++) {
+			var track = tracks[i];
+			if (timer > track.length) {
+				continue;
 			}
-		})
+			if (track.charAt(timer) == '1') {
+				var audio = new Audio(audios[i] + ".wav");
+				audio.play();
+			}
+		}
 	}
 
 	function resetInterval() {
-		clearInterval(interval)
+		clearInterval(interval);
 		interval = setInterval(appendTimer, 60000/(bpm*bpb));
 	}
 
 	onMount(() => {
-		resetInterval()
+		resetInterval();
 		return () => {
-			clearInterval(interval)
+			clearInterval(interval);
 		}
 	})
 
@@ -52,7 +71,7 @@
 <h1>Welcome to DrumHero!</h1>
 
 <p>Time: {timer}</p>
-<button on:click={resetTimer}>Reset timer</button>
+<button on:click={togglePlaying}>{playing ? "Stop beat!" : "Play beat!"}</button>
 <p>BPM:</p>
 <input bind:value={bpm} on:change={resetInterval}>
 <p>BPB:</p>
@@ -60,14 +79,14 @@
 
 <h2>Tracks</h2>
 
-<button on:click={addTrack}>Add track!</button>
+<!-- <button on:click={addTrack}>Add track!</button> -->
 
 <ul>
-	{#each tracks as t, i}
+	{#each audios as t, i}
 		<li>
 			<p>{i}:{t}</p>
 			<input bind:value={tracks[i]}>
-			<button on:click={() => removeTrack(i)}>X</button>
+			<!-- <button on:click={() => removeTrack(i)}>X</button> -->
 		</li>
 	{/each}
 </ul>
